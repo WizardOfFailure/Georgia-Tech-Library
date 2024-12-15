b8e546c9cf8c642eb1fd3553871ff8551fc4ae93
Owendho <owendho2000@gmail.com>
2024-12-15 14:14:47 +0100

    Georgia Tech system
    Added API gateway, caching and saga pattern choreography compensation

---
+++ GeorgiaTechLibrarySystem/InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InventoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("ReceiveMessage")]
+        public IActionResult Get()
+        {
+            InventoryService inventoryService = new InventoryService();
+            inventoryService.ReceiveMessage();
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("ReceiveBasicMessage")]
+        public IActionResult GetBasicBook()
+        {
+            InventoryService inventoryService = new InventoryService();
+            inventoryService.ReceiveBasicMessage();
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("ReceivePublishSubscribeMessage")]
+        public IActionResult GetPublishSubsribeBook()
+        {
+            InventoryService inventoryService = new InventoryService();
+            inventoryService.ReceivePublishSubscribeMessage();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("ReceivePublishSubscribeSagaMessage")]
+        public IActionResult GetPublishSubsribeSagaBook()
+        {
+            InventoryService inventoryService = new InventoryService();
+            inventoryService.ReceivePublishSubscribeSagaMessage();
+            return Ok();
+        }
+    }
+}
---
+++ GeorgiaTechLibrarySystem/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        //[HttpPost(Name = "PostProduct")]
+        /*[HttpPost]
+        public IActionResult Post()
+        {
+            return Accepted(); //Change this
+        }*/
+
+        [HttpPost]
+        [Route("SendMessage")]
+        public IActionResult PostMessage()
+        {
+            ProductService productService = new ProductService();
+            productService.ProduceMessage();
+            return Accepted(); //Change this
+        }
+
+        [HttpPost]
+        [Route("SendBasicMessage")]
+        public IActionResult PostBasicMessage()
+        {
+            ProductService productService = new ProductService();
+            productService.ProduceBasicMessage();
+            return Accepted(); //Change this
+        }
+
+        [HttpPost]
+        [Route("SendPublishSubscribeMessage")]
+        public IActionResult PostPublishSubsribeMessage()
+        {
+            ProductService productService = new ProductService();
+            productService.ProducePublishSubscribeMessage();
+            return Accepted(); //Change this
+        }
+
+        [HttpPost]
+        [Route("SendPublishSubscribeSagaMessage")]
+        public IActionResult PostPublishSubsribeSagaMessage()
+        {
+            ProductService productService = new ProductService();
+            productService.ProducePublishSubscribeSagaMessage();
+            return Accepted(); //Change this
+        }
+
+    }
+}
